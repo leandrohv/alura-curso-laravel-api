c093864273c9c97eb94b3852d17b9e3d1385a9e6
Leandro Venâncio <leandrohilariovenancio@gmail.com>
2019-05-29 01:02:37 -0300

    COMMIT-001 - Curso Laravel Alura Api - Preparado ambiente, criado migration, criado controllers, criado models

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController
+{
+    public function index()
+    {
+        return Serie::all();
+    }
+}
